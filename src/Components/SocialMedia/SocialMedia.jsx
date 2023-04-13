6a50324c8108b81e71240c414a8904790bdf48ab
mizaeldouglas <mizaeldouglas2@gmail.com>
2023-04-13 20:27:49 -0300

    arrumando projeto

--- src/Components/SocialMedia/SocialMedia.jsx
+++ src/Components/SocialMedia/SocialMedia.jsx
@@ -1,15 +1,28 @@
-import styles from './SocialMedia.module.scss';
+import styles from "./SocialMedia.module.scss";
 
-import { RiLinkedinFill, RiWhatsappFill } from 'react-icons/ri'
-import { AiFillInstagram } from 'react-icons/ai'
+import { RiLinkedinFill, RiWhatsappFill } from "react-icons/ri";
+import { AiFillInstagram, AiFillGithub } from "react-icons/ai";
 
 export default function SocialMedia({ social }) {
-    const { linkedin, instagram, whatsapp } = social;
-    return (
-        <div className={styles.social}>
-            <a href={linkedin} target="_blank"> <RiLinkedinFill className={styles.icon && styles.linkedin} /> </a>
-            <a href={whatsapp} target="_blank"> <RiWhatsappFill className={styles.icon && styles.whatsapp} /> </a>
-            <a href={instagram} target="_blank"> <AiFillInstagram className={styles.icon && styles.instagram} /> </a>
-        </div>
-    )
+  const { linkedin, instagram, whatsapp } = social;
+  return (
+    <div className={styles.social}>
+      <a href={linkedin} target="_blank">
+        {" "}
+        <RiLinkedinFill className={styles.icon && styles.linkedin} />{" "}
+      </a>
+      <a href={whatsapp} target="_blank">
+        {" "}
+        <RiWhatsappFill className={styles.icon && styles.whatsapp} />{" "}
+      </a>
+      <a href={instagram} target="_blank">
+        {" "}
+        <AiFillInstagram className={styles.icon && styles.instagram} />{" "}
+      </a>
+      <a href="https://github.com/Mizaeldouglas" target="_blank">
+        {" "}
+        <AiFillGithub className={styles.icon && styles.instagram} />{" "}
+      </a>
+    </div>
+  );
 }
